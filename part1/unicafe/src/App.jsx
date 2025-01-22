7c1638fd939c0ead0a8c272b50bac50e7a9723c9
santi-b-b <sbenitezbeas@gmail.com>
2025-01-22 16:49:57 -0300

    Adding excersise 1.7 using auxiliar local variables

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,17 +5,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [avg, setAvg] = useState('aun no hay actividad')
+  const [sum, setSum] = useState(0)
+  const [pos, setPos] = useState(0)
+  const [percentage, setPercentage] = useState('aun no hay actividad')
 
   const handleClickGood = () => {
+    const sum_aux = sum + 1
+    const all_aux = all + 1
+    const pos_aux = pos + 1 
     setGood(good + 1)
+    setAll(all_aux)
+    setPos(pos_aux)
+    setSum(sum_aux)
+    setAvg(sum_aux/all_aux)
+    setPercentage('' + pos_aux/all_aux + '%')
+
   }
   const handleClickNeutral = () => {
+    const all_aux = all + 1
     setNeutral(neutral + 1)
+    setAll(all_aux)
+    setAvg(sum/all_aux)
+    setPercentage('' + pos/all_aux + '%')
   }
   const handleClickBad = () => {
+    const sum_aux = sum - 1
+    const all_aux = all + 1
     setBad(bad + 1)
+    setAll(all_aux)
+    setSum(sum_aux)
+    setAvg(sum_aux/all_aux)
+    setPercentage('' + pos/all_aux + '%')
   }
-
 
   return (
     <div>
@@ -30,9 +53,12 @@ const App = () => {
         bad
       </button>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {avg}</p>
+      <p>positive: {percentage}</p>
     </div>
   )
 }
