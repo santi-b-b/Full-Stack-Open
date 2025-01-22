bddbb6283e21c9f1df613eb28e005375fea4e3d1
santi-b-b <sbenitezbeas@gmail.com>
2025-01-22 14:17:17 -0300

    Playing with the examples

--- part1/examples/src/App.jsx
+++ part1/examples/src/App.jsx
@@ -2,32 +2,48 @@ import { useState } from "react"
 
 
 const App = () => {
-    const [left, setLeft] = useState(0) // Se setea el estado para left y su metodo set setLeft
-    const [right, setRight] = useState(0)
-  
-    const [allClicks, setAll] = useState([]) // Se setea un estado con un objeto que es un arreglo vacío
-  
-  
-    const handleLeftClick = () => { // Este manejador suma 1 a la variable left cada vez que es llamado y al arreglo allClicks le interta una 'L' de left al final
-      setAll(allClicks.concat('L'))
-      setLeft(left + 1)
-    }
-  
-  
-    const handleRightClick = () => {
-      setAll(allClicks.concat('R'))
-      setRight(right + 1)
-    }
-  
-    return ( // me imagino que la funcion join de un arreglo une todos los elementos de este intercalando con el parámetro que recibe
+
+const History = (props) => { // este método se encarga de manejar la información que se va a desplegar en base a los estados de la app
+  if (props.allClicks.length === 0) {
+    return (
       <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-  
-        <p>{allClicks.join(' ')}</p> 
+        the app is used by pressing the buttons
       </div>
     )
   }
+  return (
+    <div>
+      <p>button press history: {props.allClicks.join(' ')}</p>
+      <p>Left:{props.left} </p>
+      <p>right:{props.right} </p>
+    </div>
+  )
+}
+
+const [left, setLeft] = useState(0)
+const [right, setRight] = useState(0)
+const [allClicks, setAll] = useState([])
+const [total, setTotal] = useState(0)
+
+const handleLeftClick = () => {
+  setAll(allClicks.concat('L'))
+  setLeft(left + 1)
+  setTotal(left + right)
+}
+
+const handleRightClick = () => {
+  setAll(allClicks.concat('R'))
+  setRight(right + 1)
+  setTotal(left + right)
+}
+
+  return (
+    <div>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+
+      <History allClicks={allClicks} left = {left} right = {right} />
+    </div>
+  )
+}
 export default App
