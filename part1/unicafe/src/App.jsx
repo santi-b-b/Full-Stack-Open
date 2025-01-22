2d7b0549c8ed90c38f70882e237db8e7d09a20c0
santi-b-b <sbenitezbeas@gmail.com>
2025-01-22 17:20:31 -0300

    Adding exercise 1.8 refactoring

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,43 +1,54 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  console.log(props.all)
+  if(props.all > 0){
+    const avg = props.sum / props.all
+    const percentage = props.good / props.all
+    return(
+      <div>
+        <p>average: {avg}</p>
+        <p>positive: {percentage} %</p>
+      </div>
+    )
+  }
+  else{
+    return (
+      <div>
+        <p>Aun no hay clicks para generar estadisticas</p>
+      </div>
+    )
+  }
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
-  const [avg, setAvg] = useState('aun no hay actividad')
   const [sum, setSum] = useState(0)
-  const [pos, setPos] = useState(0)
-  const [percentage, setPercentage] = useState('aun no hay actividad')
 
   const handleClickGood = () => {
     const sum_aux = sum + 1
     const all_aux = all + 1
-    const pos_aux = pos + 1 
     setGood(good + 1)
     setAll(all_aux)
-    setPos(pos_aux)
     setSum(sum_aux)
-    setAvg(sum_aux/all_aux)
-    setPercentage('' + pos_aux/all_aux + '%')
 
   }
   const handleClickNeutral = () => {
     const all_aux = all + 1
     setNeutral(neutral + 1)
     setAll(all_aux)
-    setAvg(sum/all_aux)
-    setPercentage('' + pos/all_aux + '%')
   }
   const handleClickBad = () => {
     const sum_aux = sum - 1
     const all_aux = all + 1
+    console.log(all_aux)
     setBad(bad + 1)
     setAll(all_aux)
     setSum(sum_aux)
-    setAvg(sum_aux/all_aux)
-    setPercentage('' + pos/all_aux + '%')
   }
 
   return (
@@ -56,9 +67,7 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>average: {avg}</p>
-      <p>positive: {percentage}</p>
+      <Statistics all = {all} good = {good} sum = {sum}/>
     </div>
   )
 }
