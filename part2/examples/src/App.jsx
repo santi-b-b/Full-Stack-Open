901d6ef39a2f71880e479b9eb0ef8945628f7084
santi-b-b <sbenitezbeas@gmail.com>
2025-05-23 13:00:17 +0200

    Adding examples of part 2.e-1

--- part2/examples/src/App.jsx
+++ part2/examples/src/App.jsx
@@ -1,7 +1,22 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
-import axios from 'axios'
+import './index.css'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
 
 
 const App = () => {
@@ -9,6 +24,8 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('')
+
   console.log(notes)
 
   useEffect(() => {
@@ -56,9 +73,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -71,6 +91,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div> 
       <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -92,6 +113,7 @@ const App = () => {
         />
         <button type = "submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
