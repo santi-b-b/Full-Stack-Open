ff759b7e104594d3e3851c4f7058f47e67724ad1
santi-b-b <sbenitezbeas@gmail.com>
2025-07-03 17:20:46 +0200

    Adding exercises 3.9-3.11 phonebook to internet

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 var morgan = require('morgan')
 const app = express()
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 
 morgan.token('body', function (req) {
@@ -8,10 +11,6 @@ morgan.token('body', function (req) {
     return JSON.stringify(req.body);
   }
 });
-
-
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
     { 
@@ -103,7 +102,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
